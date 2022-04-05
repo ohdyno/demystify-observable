0c3f0351cf5d8cf59b2bab1ab6323293a3e2e852
Xing Zhou <xing@xingzhou.me>
2022-04-04 22:20:31 -0400

    feat: update observable to take in a provider

--- observable.test.js
+++ observable.test.js
@@ -1,6 +1,12 @@
 class Observable {
+    provider;
+
+    constructor(provider = (subscribe) => subscribe(1)) {
+        this.provider = provider;
+    }
+
     subscribe(handler) {
-        handler(1)
+        this.provider(handler)
     }
 }
 
@@ -8,6 +14,17 @@ describe('observable', () => {
     it('can be created to always give subscribers the number 1 once', (done) => {
         new Observable().subscribe(n => {
             expect(n).toEqual(1);
+            done();
+        })
+    })
+
+    it('can be created with a provider function', (done) => {
+        function provider(subscriber) {
+            subscriber(12);
+        }
+
+        new Observable(provider).subscribe(n => {
+            expect(n).toEqual(12);
             done();
         })
     })
